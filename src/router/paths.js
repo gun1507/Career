74595f7e26fc6b7bad769604d5041cfff45e6b83
DESKTOP-JDB7H0Q\gun15 <kgun1507@gmail.com>
2021-09-03 23:32:42 +0900

    commit : 개발환경 세팅

--- src/router/paths.js
+++ src/router/paths.js
@@ -3,41 +3,41 @@ export default [
   {
     path: '/',
     name: 'Login',
-    component: () => import('../views/login/Index'),
+    component: () => import('../views/login/index'),
   },
   {
     path: '/main',
-    component: () => import('../views/main/Index'),
+    component: () => import('../views/main/index'),
     children: [
       {
         name: 'History',
         path: '/history',
-        component: () => import('../views/history/History')
+        component: () => import('../views/history/history')
       },
       {
         name: '사용자이력',
         path: '/user_history',
-        component: () => import('../views/userHistory/UserHistory')
+        component: () => import('../views/userHistory/userHistory')
       },
       {
         name: 'log',
         path: '/log',
-        component: () => import('../views/admin/log/Log')
+        component: () => import('../views/admin/log/log')
       },
       {
         name: 'model',
         path: '/model',
-        component: () => import('../views/admin/model/Model')
+        component: () => import('../views/admin/model/model')
       },
       {
         name: '사용자권한',
         path: '/auth',
-        component: () => import('../views/admin/auth/Auth')
+        component: () => import('../views/admin/auth/auth')
       },
       {
         name: 'help',
         path: '/help',
-        component: () => import('../views/help/Help')
+        component: () => import('../views/help/help')
       },
 
     ]
